f7f189e583b70406816448f697fb475495a2c9d7
Eric <eric.gravier@outlook.com>
2022-01-25 17:53:59 +0100

    sécuritée dotenv pour token

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,13 +2,14 @@
 //--------------------------------------------------
 //---------- verification du token ------------------
 //----------------------------------------------------
+require('dotenv').config();    // recuperation des variables d'environnements 
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
     // recuperation de la chaine du header ( apres mot spread : spread chainecrypté)
     const token = req.headers.authorization.split(' ')[1];    // split sur l'espace apres spread   
-    const decodedToken = jwt.verify(token, 'chaine-secrete'); // verification du token
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_STRING); // verification du token
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {    // userId present et userId different du userId token
       throw 'Invalid user ID';
