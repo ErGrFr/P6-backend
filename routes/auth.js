9e68d8c9eb7bfcbe3de386de25c0517bb8420ca1
Eric <eric.gravier@outlook.com>
2022-01-26 11:24:25 +0100

    Oups oublie require dans route

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const authCtrl = require('../controllers/auth');
+const checkpassword = require('../middleware/checkpassword');
 
 router.post('/login', authCtrl.login);
 router.post('/signup', checkpassword, authCtrl.signup);
